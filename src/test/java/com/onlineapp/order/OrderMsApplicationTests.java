1d080eaa4f2e04feebea27eae35228c82890219c
kavitavkokate <147817308+kavitavkokate@users.noreply.github.com>
2023-10-30 16:24:26 +0000

    Added unit tests

--- src/test/java/com/onlineapp/order/OrderMsApplicationTests.java
+++ src/test/java/com/onlineapp/order/OrderMsApplicationTests.java
@@ -1,13 +1,12 @@
-package com.onlineapp.order;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class OrderMsApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+/*
+ * package com.onlineapp.order;
+ * 
+ * import org.junit.jupiter.api.Test; import
+ * org.springframework.boot.test.context.SpringBootTest;
+ * 
+ * @SpringBootTest class OrderMsApplicationTests {
+ * 
+ * @Test void contextLoads() { }
+ * 
+ * }
+ */
